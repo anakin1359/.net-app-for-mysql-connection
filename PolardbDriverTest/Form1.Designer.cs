71ca30ac23b8e2ca50db24e806dd7956366d4fae
takeuchima <96402889+takeuchima@users.noreply.github.com>
2022-01-26 00:50:12 +0900

    setup

---
+++ PolardbDriverTest/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PolardbDriverTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.DataTableReadButton = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// DataTableReadButton
+			// 
+			this.DataTableReadButton.Location = new System.Drawing.Point(23, 12);
+			this.DataTableReadButton.Name = "DataTableReadButton";
+			this.DataTableReadButton.Size = new System.Drawing.Size(219, 33);
+			this.DataTableReadButton.TabIndex = 0;
+			this.DataTableReadButton.Text = "DataTable Read";
+			this.DataTableReadButton.UseVisualStyleBackColor = true;
+			this.DataTableReadButton.Click += new System.EventHandler(this.DataTableReadButton_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(260, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 62;
+			this.dataGridView1.RowTemplate.Height = 33;
+			this.dataGridView1.Size = new System.Drawing.Size(590, 392);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(862, 428);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.DataTableReadButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button DataTableReadButton;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
---
+++ PolardbDriverTest/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PolardbDriverTest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			StartPosition = FormStartPosition.CenterScreen;
+		}
+
+		private void DataTableReadButton_Click(object sender, EventArgs e)
+		{
+			// 本番環境模倣
+			// MySQL接続用
+			//string connStr = "server=polardb-mysql-dev.rwlb.singapore.rds.aliyuncs.com;user id=snadmin;password=Gorilla0#;database=isono_database";
+			//MySqlConnection connection = new MySqlConnection(connStr);
+
+			// 検証用
+			var builder = new MySqlConnectionStringBuilder();
+			builder.Server = "polardb-mysql-dev.rwlb.singapore.rds.aliyuncs.com";
+			builder.Database = "isono_database";
+			builder.UserID = "snadmin";
+			builder.Password = "Gorilla0#";
+			//builder.IntegratedSecurity = true;
+			var ConnectionString = builder.ToString();
+
+			var sql = @"select * from gorilla_table";
+
+			DataTable dataTable = new DataTable();
+			using (var connection = new MySqlConnection(ConnectionString))
+			using (var adapter = new MySqlDataAdapter(sql, connection))
+			{
+				connection.Open();
+				adapter.Fill(dataTable);
+			}
+			
+			dataGridView1.DataSource = dataTable;
+		}
+	}
+}
